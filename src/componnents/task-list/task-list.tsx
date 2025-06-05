b7bd0dd56e62eb6a6a5136660f2b477764fc1b7e
Itschak Shteren <tomerst77@gmail.com>
2025-06-05 16:26:23 +0300

    feat: create task list props

--- src/componnents/task-list/task-list.tsx
+++ src/componnents/task-list/task-list.tsx
@@ -1,3 +1,16 @@
-export const TaskList = () => {
+import type { FC } from "react";
+import type { Task } from "../../types";
+
+type TaskListProps = {
+  tasks: Task[];
+  setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
+  isFulfilled: boolean;
+};
+
+export const TaskList: FC<TaskListProps> = ({
+  tasks,
+  setTasks,
+  isFulfilled,
+}: TaskListProps) => {
   return <div></div>;
 };
