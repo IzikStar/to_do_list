cc610b3f4157e9fe07c5c3d1687ed9c74f2da477
Itschak Shteren <tomerst77@gmail.com>
2025-06-05 16:26:52 +0300

    feat: create main page skeleton

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,20 @@
-import "./App.css";
+import styles from "./App.module.css";
+import type { Task } from "../types";
+import { useState } from "react";
+import { TaskList } from "../componnents/task-list";
 
 export const App = () => {
-  return <></>;
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  return (
+    <body className={styles.body}>
+      <header className={styles.header}>To-Do List</header>
+      <section>
+        <TaskList tasks={tasks} setTasks={setTasks} isFulfilled={false} />
+        <TaskList tasks={tasks} setTasks={setTasks} isFulfilled={true} />
+      </section>
+      // modal
+    </body>
+  );
 };
