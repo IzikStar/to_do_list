19ceb3803aff4b51d70769e6659904a2dfd5e477
itschak shteren <itschakme@gmail.com>
2025-06-05 21:21:05 +0300

    feat: wrap app wuth a modal provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./app";
 import { TasksProvider } from "./context/tasks-context";
+import { ModalProvider } from "./context/modal-context";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <TasksProvider>
-      <App />
+      <ModalProvider>
+        <App />
+      </ModalProvider>
     </TasksProvider>
   </StrictMode>
 );
