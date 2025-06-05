0122477b42bda0265b95bc495d4903907b5622ab
itschak shteren <itschakme@gmail.com>
2025-06-05 20:56:59 +0300

    feat: wrap app with tasks provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./app";
+import { TasksProvider } from "./context/tasks-context";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <TasksProvider>
+      <App />
+    </TasksProvider>
   </StrictMode>
 );
