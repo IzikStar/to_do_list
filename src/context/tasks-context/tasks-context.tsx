2ef2fd65c2b75c427c1797a57110184ae03e8079
itschak shteren <itschakme@gmail.com>
2025-06-08 17:30:05 +0300

    feat: add method to change task state and fix remove task method

--- src/context/tasks-context/tasks-context.tsx
+++ src/context/tasks-context/tasks-context.tsx
@@ -1,11 +1,12 @@
 import { createContext } from "react";
-import type { Task } from "../../types";
+import type { ListTypes, Task } from "../../types";
 
 export interface TasksContextType {
   tasks: Task[];
   addTask: (newTask: Task) => void;
   removeTask: (taskId: Task["id"]) => void;
   editTask: (newTaskDetails: Task) => void;
+  changeTaskState: (task: Task, newState: ListTypes) => void;
 }
 
 export const TasksContext = createContext<TasksContextType | null>(null);
--- src/context/tasks-context/tasks-provider.tsx
+++ src/context/tasks-context/tasks-provider.tsx
@@ -1,6 +1,6 @@
 import { useState, type FC, type ReactNode } from "react";
 import { TasksContext } from "./tasks-context";
-import type { Task } from "../../types";
+import type { ListTypes, Task } from "../../types";
 
 export const TasksProvider: FC<{
   children: ReactNode[] | ReactNode;
@@ -10,12 +10,26 @@ export const TasksProvider: FC<{
   const addTask = (newTask: Task) => setTasks((prev) => [...prev, newTask]);
 
   const removeTask = (taskId: Task["id"]) =>
-    setTasks((prev) => prev.filter((task: Task) => task.id !== taskId));
+    setTasks((prev) =>
+      prev
+        .filter((task: Task) => task.id !== taskId)
+        .map((task: Task, index: number) => {
+          return { ...task, id: index };
+        })
+    );
 
   const editTask = (newTask: Task) =>
     setTasks((prev) =>
       prev.map((task: Task) => (task.id === newTask.id ? newTask : task))
     );
+
+  const changeTaskState = (task: Task, newState: ListTypes) => {
+    setTasks((prev) =>
+      prev.map((currentTask: Task) =>
+        currentTask.id === task.id ? { ...task, type: newState } : currentTask
+      )
+    );
+  };
 
   return (
     <TasksContext.Provider
@@ -24,6 +38,7 @@ export const TasksProvider: FC<{
         addTask,
         removeTask,
         editTask,
+        changeTaskState,
       }}
     >
       {children}
