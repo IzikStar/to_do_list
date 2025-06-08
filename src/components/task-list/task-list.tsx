e8ac882052baf396b47bb357e42f120afcb98f6d
itschak shteren <itschakme@gmail.com>
2025-06-08 15:10:34 +0300

    fix: fix CR comments

--- src/components/task-list/task-list.tsx
+++ src/components/task-list/task-list.tsx
@@ -1,9 +1,10 @@
-import type { FC } from "react";
+import type { FC, ReactElement } from "react";
 import { ListTypes, type Task as TaskType } from "../../types";
 import { useTasks } from "../../context";
 import { Task } from "../task";
 import { useModal } from "../../context";
 import styles from "./task-list.module.css";
+import { IconButton } from "../../components";
 
 type TaskListProps = {
   type: ListTypes;
@@ -15,17 +16,20 @@ export const TaskList: FC<TaskListProps> = ({ type }: TaskListProps) => {
 
   const filteredTasks = tasks.filter((task: TaskType) => task.type === type);
 
+  const renderTasks = (): ReactElement[] => {
+    return filteredTasks.map((task: TaskType) => <Task task={task} />);
+  };
+
   return (
     <>
       <div className={styles.container}>
         <div className={styles.header}>{type}</div>
-        {filteredTasks.map((task: TaskType) => (
-          <Task task={task} />
-        ))}
+        
+        {renderTasks()}
 
         {type === ListTypes.TODO && (
-          <img
-            src="/src/assets/add-task-icon.svg"
+          <IconButton
+            iconSrc="/src/assets/add-task-icon.svg"
             onClick={openCreateMode}
             className={styles.icon}
           />
